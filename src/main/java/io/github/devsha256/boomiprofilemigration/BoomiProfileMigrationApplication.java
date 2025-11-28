90985f2b31a46dbe0c2381fdb5f3797a132e0ad9
devsha256 <dev.hsaddam@gmail.com>
2025-11-29 02:08:01 +0530

    Add spring boot app

---
+++ src/main/java/io/github/devsha256/boomiprofilemigration/BoomiProfileMigrationApplication.java
@@ -0,0 +1,13 @@
+package io.github.devsha256.boomiprofilemigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomiProfileMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoomiProfileMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/devsha256/boomiprofilemigration/BoomiProfileMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devsha256.boomiprofilemigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoomiProfileMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
